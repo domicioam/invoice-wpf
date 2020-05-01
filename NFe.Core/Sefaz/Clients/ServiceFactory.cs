3b97a914fa7ce538b034d08cd7b87e15e2f1e79c
Domício Medeiros <domicioam@gmail.com>
2020-05-01 16:05:23 +0100

    Using transforms to simplify service factory.

--- NFe.Core/Sefaz/Clients/ServiceFactory.cs
+++ NFe.Core/Sefaz/Clients/ServiceFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Evento = NFe.Core.NFeRecepcaoEvento4;
 
 namespace NFe.Core.NotasFiscais
@@ -18,91 +19,47 @@ namespace NFe.Core.NotasFiscais
     {
         public Service GetService(Modelo modelo, Ambiente ambiente, Servico servico, CodigoUfIbge UF, X509Certificate2 certificado)
         {
-            string endpoint = "";
-
+            string endpoint;
             if (modelo == Modelo.Modelo55)
             {
-                if (ambiente == Ambiente.Homologacao)
+                switch (servico)
                 {
-                    switch (servico)
-                    {
-                        case Servico.CANCELAMENTO:
-                            endpoint = "RecepcaoEventoSoapHomNfe";
-                            return GerarServicoCancelamento(UF, certificado, endpoint);
-                        case Servico.INUTILIZACAO:
-                            endpoint = "NfeInutilizacao2Homologacao";
-                            return GerarServicoInutilizacao(UF, certificado, endpoint);
-                        case Servico.AUTORIZACAO:
-                            endpoint = "NfeAutorizacaoSoapHom";
-                            return GerarServicoAutorizacao(UF, certificado, endpoint);
-                        case Servico.RetAutorizacao:
-                            endpoint = "NfeRetAutorizacaoSoapHom";
-                            return GerarServicoRetAutorizacao(UF, certificado, endpoint);
-                    }
-                }
-                else
-                {
-                    switch (servico)
-                    {
-                        case Servico.CANCELAMENTO:
-                            endpoint = "RecepcaoEventoSoapProdNfe";
-                            return GerarServicoCancelamento(UF, certificado, endpoint);
-                        case Servico.INUTILIZACAO:
-                            endpoint = "NfeInutilizacao2Producao";
-                            return GerarServicoInutilizacao(UF, certificado, endpoint);
-                        case Servico.AUTORIZACAO:
-                            endpoint = "NfeAutorizacaoSoapProd";
-                            return GerarServicoAutorizacao(UF, certificado, endpoint);
-                        case Servico.RetAutorizacao:
-                            endpoint = "NfeRetAutorizacaoSoapProd";
-                            return GerarServicoRetAutorizacao(UF, certificado, endpoint);
-                    }
+                    case Servico.CANCELAMENTO:
+                        endpoint = "RecepcaoEventoSoapNfe";
+                        return GerarServicoCancelamento(UF, certificado, endpoint);
+                    case Servico.INUTILIZACAO:
+                        endpoint = "NfeInutilizacao2";
+                        return GerarServicoInutilizacao(UF, certificado, endpoint);
+                    case Servico.AUTORIZACAO:
+                        endpoint = "NfeAutorizacaoSoap";
+                        return GerarServicoAutorizacao(UF, certificado, endpoint);
+                    case Servico.RetAutorizacao:
+                        endpoint = "NfeRetAutorizacaoSoap";
+                        return GerarServicoRetAutorizacao(UF, certificado, endpoint);
                 }
             }
 
             if (modelo == Modelo.Modelo65)
             {
-                if (ambiente == Ambiente.Homologacao)
+                switch (servico)
                 {
-                    switch (servico)
-                    {
-                        case Servico.CANCELAMENTO:
-                            endpoint = "RecepcaoEventoSoapHomNfce";
-                            return GerarServicoCancelamento(UF, certificado, endpoint);
+                    case Servico.CANCELAMENTO:
+                        endpoint = "RecepcaoEventoSoapNfce";
+                        return GerarServicoCancelamento(UF, certificado, endpoint);
 
-                        case Servico.INUTILIZACAO:
-                            endpoint = "NfceInutilizacao2Homologacao";
-                            return GerarServicoInutilizacao(UF, certificado, endpoint);
-                        case Servico.AUTORIZACAO:
-                            endpoint = "NfceAutorizacaoSoapHom";
-                            return GerarServicoAutorizacao(UF, certificado, endpoint);
-                        case Servico.RetAutorizacao:
-                            endpoint = "NfceRetAutorizacaoSoapHom";
-                            return GerarServicoRetAutorizacao(UF, certificado, endpoint);
-                    }
-                }
-                else
-                {
-                    switch (servico)
-                    {
-                        case Servico.CANCELAMENTO:
-                            endpoint = "RecepcaoEventoSoapProdNfce";
-                            return GerarServicoCancelamento(UF, certificado, endpoint);
-
-                        case Servico.INUTILIZACAO:
-                            endpoint = "NfceInutilizacao2Producao";
-                            return GerarServicoInutilizacao(UF, certificado, endpoint);
-                        case Servico.AUTORIZACAO:
-                            endpoint = "NfceAutorizacaoSoapProd";
-                            return GerarServicoAutorizacao(UF, certificado, endpoint);
-                        case Servico.RetAutorizacao:
-                            endpoint = "NfceRetAutorizacaoSoapProd";
-                            return GerarServicoRetAutorizacao(UF, certificado, endpoint);
-                    }
+                    case Servico.INUTILIZACAO:
+                        endpoint = "NfceInutilizacao2";
+                        return GerarServicoInutilizacao(UF, certificado, endpoint);
+                    case Servico.AUTORIZACAO:
+                        endpoint = "NfceAutorizacaoSoap";
+                        return GerarServicoAutorizacao(UF, certificado, endpoint);
+                    case Servico.RetAutorizacao:
+                        endpoint = "NfceRetAutorizacaoSoap";
+                        return GerarServicoRetAutorizacao(UF, certificado, endpoint);
                 }
             }
 
-            return null;
+            throw new ArgumentException("Configuration not found!");
         }
 
         private static Service GerarServicoAutorizacao(CodigoUfIbge UF, X509Certificate2 certificado, string endpoint)
