ecd8c11c8fe801d0ff9f60df8b2a1bb8365f279b
Domício Medeiros <domicioam@gmail.com>
2020-11-01 20:28:37 +0000

    Adding unit tests for CofinsAliquotaDiferenciada

--- NFe.Core.UnitTests/Sefaz/CofinsCreatorTests.cs
+++ NFe.Core.UnitTests/Sefaz/CofinsCreatorTests.cs
@@ -41,5 +41,44 @@ namespace NFe.Core.UnitTests.Sefaz
             Assert.Equal(aliquota.ToString(), detCofins01.pCOFINS);
             Assert.Equal(TNFeInfNFeDetImpostoCOFINSCOFINSAliqCST.Item01, detCofins01.CST);
         }
+
+        [Fact]
+        public void Should_create_cofins_aliquota_diferenciada_with_empty_fields()
+        {
+            var icmsCreator = new CofinsCreator();
+
+            Imposto imposto = new CofinsAliquotaDiferenciada(0, 0);
+            var detImposto = (TNFeInfNFeDetImpostoCOFINS)icmsCreator.Create(imposto);
+            var detCofins02 = (TNFeInfNFeDetImpostoCOFINSCOFINSAliq)detImposto.Item;
+
+            Assert.Null(detCofins02.pCOFINS);
+            Assert.Null(detCofins02.vBC);
+            Assert.Null(detCofins02.vCOFINS);
+            Assert.Equal(TNFeInfNFeDetImpostoCOFINSCOFINSAliqCST.Item02, detCofins02.CST);
+        }
+
+        [Theory]
+        [InlineData(0.1, 0.2)]
+        public void Should_create_cofins_aliquota_diferenciada_with_correct_values(decimal baseCalculo, decimal aliquota)
+        {
+            var cofinsCreator = new CofinsCreator();
+
+            Imposto imposto = new CofinsAliquotaDiferenciada(baseCalculo, aliquota);
+            var detImposto = (TNFeInfNFeDetImpostoCOFINS)cofinsCreator.Create(imposto);
+            var detCofins02 = (TNFeInfNFeDetImpostoCOFINSCOFINSAliq)detImposto.Item;
+
+            Assert.Equal(baseCalculo.ToString(), detCofins02.vBC);
+            Assert.Equal(aliquota.ToString(), detCofins02.pCOFINS);
+            Assert.Equal(TNFeInfNFeDetImpostoCOFINSCOFINSAliqCST.Item02, detCofins02.CST);
+        }
+
+        [Fact]
+        public void Should_throw_exception_when_cofins_type_is_wrong()
+        {
+            var cofinsCreator = new CofinsCreator();
+            Imposto imposto = new IcmsCobradoAnteriormentePorSubstituicaoTributaria(0, 0,0,0,0,0, OrigemMercadoria.Nacional);
+
+            Assert.Throws<ArgumentException>(() => cofinsCreator.Create(imposto));
+        }
     }
 }
