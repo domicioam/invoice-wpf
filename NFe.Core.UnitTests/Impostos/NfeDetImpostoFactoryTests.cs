931f94120c8bf2e69121690db499b18b9c6a7688
Domício Medeiros <domicioam@gmail.com>
2020-11-15 19:58:01 +0000

    Creating unit test for NfeDetImpostoFactory

---
+++ NFe.Core.UnitTests/Impostos/NfeDetImpostoFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NFe.Core.Sefaz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NFe.Core.UnitTests.Impostos
+{
+    public class NfeDetImpostoFactoryTests
+    {
+        [Fact]
+        public void test()
+        {
+            var mock = new Mock<ImpostoCreatorFactory>().Object;
+            var factory = new NfeDetImpostoFactory(mock);
+        }
+    }
+}
