3e7619a30205e249ea4ba016ac9bd46996e99526
Domício Medeiros <domicioam@gmail.com>
2020-11-15 10:41:11 +0000

    Adding unit test for icms 41.

---
+++ NFe.Core.UnitTests/IcmsNaoTributadoTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NFe.Core.UnitTests
+{
+    public class IcmsNaoTributadoTests
+    {
+        [Fact]
+        public void Should_create_correctly_when_fields_valid()
+        {
+            var desoneracao = new DesoneracaoIcms(10, MotivoDesoneracao.ProdutorAgropecuario);
+            var icms41 = new IcmsNaoTributado(desoneracao, OrigemMercadoria.Nacional);
+
+            Assert.Equal(Icms.CstEnum.CST41, icms41.Cst);
+            Assert.Equal(OrigemMercadoria.Nacional, icms41.Origem);
+            Assert.Equal(10, icms41.DesoneracaoIcms.ValorDesonerado);
+        }
+    }
+}
--- NFe.Core/NotasFiscais/DesoneracaoIcms.cs
+++ NFe.Core/NotasFiscais/DesoneracaoIcms.cs
@@ -2,8 +2,16 @@
 
 namespace NFe.Core
 {
+    /// <summary>
+    /// Classe para cálculo da desoneração do ICMS. Atualmente o valor é inserido manualmente pelo usuário. Ver a possibilidade de fazer os cálculos usando a base de cálculo simples e por fora.
+    /// </summary>
     internal class DesoneracaoIcms
     {
+        /// <summary>
+        /// O valor desonerado deve ser calculado manualmente e inserido na aplicação.
+        /// </summary>
+        /// <param name="valorDesonerado"></param>
+        /// <param name="motivoDesoneracao"></param>
         public DesoneracaoIcms(decimal valorDesonerado, MotivoDesoneracao motivoDesoneracao)
         {
             if (valorDesonerado > 0 && motivoDesoneracao == MotivoDesoneracao.NaoPreenchido)
