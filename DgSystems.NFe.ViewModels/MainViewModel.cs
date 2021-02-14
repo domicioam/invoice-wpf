28f57bc65a406d2af2ae13013696b6ceb4b6bc5a
Domício Medeiros <domicioam@gmail.com>
2021-02-14 16:14:14 +0000

    ...

--- DgSystems.NFe.ViewModels/MainViewModel.cs
+++ DgSystems.NFe.ViewModels/MainViewModel.cs
@@ -1,26 +1,17 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using System.Windows.Input;
-using System;
-using System.Globalization;
-using System.Linq;
-using NFe.Core.Utils.Zip;
-using System.Net.Mail;
-using System.Net;
-using System.Net.Mime;
-using System.Threading.Tasks;
-using NFe.Repository.Repositories;
-using NFe.Core.Utils;
 using NFe.Core.NotasFiscais.Services;
 using NFe.WPF.Utils;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EmissorNFe.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
         private const int diaParaEnvio = 2;
-        private MailManager _mailManager;
-        private ModoOnlineService _modoOnlineService;
+        private readonly MailManager _mailManager;
+        private readonly ModoOnlineService _modoOnlineService;
 
         public ICommand LoadedCmd { get; set; }
 
