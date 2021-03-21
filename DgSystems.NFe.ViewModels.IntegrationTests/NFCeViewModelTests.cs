d6f2fcda439a0d3f78022adcf4c1bab7ba2899cc
Domício Medeiros <domicioam@gmail.com>
2021-03-21 18:42:01 +0000

    Creating integration test for NFCeViewModel

---
+++ DgSystems.NFe.ViewModels.IntegrationTests/NFCeViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DgSystems.NFe.ViewModels.IntegrationTests
+{
+    public class NFCeViewModelTests
+    {
+        [Fact]
+        public void Deve_enviar_nota_fiscal_quando_campos_válidos()
+        {
+
+        }
+    }
+}
