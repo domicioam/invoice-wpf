54013a13255a4d39d6bc2b33e150440f12b33af3
Domício Medeiros <domicioam@gmail.com>
2021-05-16 17:17:35 +0100

    Add class to integrate danfe nfce

---
+++ DgSystems.NFe.NotaFiscal.Reports/ImprimirDanfeNfceService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DgSystems.NFe.NotaFiscal.Reports
+{
+    public class ImprimirDanfeNfceService
+    {
+        // send message from NotaFiscal bounded context to Reports bounded context
+    }
+}
