14887560635abd5686aebf13d8b79377768e2bc2
Domício Medeiros <domicioam@gmail.com>
2021-09-15 10:09:40 +0100

    ...

--- DgSystems.NFe.NotaFiscal.Services/ConsultarNotaFiscalService.cs
+++ DgSystems.NFe.NotaFiscal.Services/ConsultarNotaFiscalService.cs
@@ -12,11 +12,11 @@ namespace NFe.Core.NotasFiscais.Sefaz.NfeConsulta2
 {
     public class ConsultarNotaFiscalService : IConsultarNotaFiscalService
     {
-        private SefazSettings _sefazSettings;
+        private readonly SefazSettings sefazSettings;
 
         public ConsultarNotaFiscalService(SefazSettings sefazSettings)
         {
-            _sefazSettings = sefazSettings;
+            this.sefazSettings = sefazSettings;
         }
 
         public MensagemRetornoConsulta ConsultarNotaFiscal(string chave, string codigoUf, X509Certificate2 certificado, Modelo modelo)
@@ -31,7 +31,7 @@ namespace NFe.Core.NotasFiscais.Sefaz.NfeConsulta2
             var parametro = new TConsSitNFe
             {
                 chNFe = chave,
-                tpAmb = _sefazSettings.Ambiente == Ambiente.Homologacao ? Envio.TAmb.Item2 : Envio.TAmb.Item1,
+                tpAmb = sefazSettings.Ambiente == Ambiente.Homologacao ? Envio.TAmb.Item2 : Envio.TAmb.Item1,
                 versao = Envio.TVerConsSitNFe.Item400,
                 xServ = TConsSitNFeXServ.CONSULTAR
             };
@@ -44,23 +44,16 @@ namespace NFe.Core.NotasFiscais.Sefaz.NfeConsulta2
             var soapClient = new NFeConsultaProtocolo4SoapClient(endpoint);
             soapClient.ClientCredentials.ClientCertificate.Certificate = certificado;
             XmlNode result = soapClient.nfeConsultaNF(node);
-
             var retornoConsulta = (TRetConsSitNFe)XmlUtil.Deserialize<TRetConsSitNFe>(result.OuterXml);
+            
+            if (retornoConsulta.cStat != "100") return new MensagemRetornoConsulta { IsEnviada = false };
 
-            MensagemRetornoConsulta mensagemRetorno = new MensagemRetornoConsulta();
-
-            if (retornoConsulta.cStat == "100")
+            return new MensagemRetornoConsulta
             {
-                mensagemRetorno.IsEnviada = true;
-                mensagemRetorno.Protocolo = new Protocolo(retornoConsulta.protNFe);
-                mensagemRetorno.DhAutorizacao = retornoConsulta.protNFe.infProt.dhRecbto; //não é a hora da autorização
-            }
-            else
-            {
-                mensagemRetorno.IsEnviada = false;
-            }
-
-            return mensagemRetorno;
+                IsEnviada = true,
+                Protocolo = new Protocolo(retornoConsulta.protNFe),
+                DhAutorizacao = retornoConsulta.protNFe.infProt.dhRecbto //não é a hora da autorização
+            };
         }
     }
 }
