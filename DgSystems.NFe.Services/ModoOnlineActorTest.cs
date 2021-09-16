703a78d169ef1965d44c202f19696c9b908ccdaf
Domício Medeiros <domicioam@gmail.com>
2021-09-16 18:46:35 +0100

    Test start with service up

--- DgSystems.NFe.Services/ModoOnlineActorTest.cs
+++ DgSystems.NFe.Services/ModoOnlineActorTest.cs
@@ -16,6 +16,7 @@ using NFe.Core.Sefaz;
 using Akka.Actor;
 using DgSystems.NFe.Services.Actors;
 using NFe.Core.Cadastro.Configuracoes;
+using NFe.Core.Domain;
 
 namespace DgSystems.NFe.Services.UnitTests
 {
@@ -50,6 +51,39 @@ namespace DgSystems.NFe.Services.UnitTests
             contingenciaActor.ExpectMsg<EmiteNFeContingenciaActor.TransmitirNFeEmContingencia>();
             configuracaoRepository.Verify(c => c.Salvar(It.Is<ConfiguracaoEntity>(config => config.IsContingencia == false)), Times.Once);
             configuracaoRepository.Verify(c => c.GetConfiguracao(), Times.Once);
+        }
+
+        [Fact]
+        public void Should_activate_modo_online_when_started_and_service_is_up()
+        {
+            // Given
+            Mock<IConfiguracaoRepository> configuracaoRepository = new Mock<IConfiguracaoRepository>();
+            Mock<IConsultaStatusServicoSefazService> consultaStatusServicoService = new Mock<IConsultaStatusServicoSefazService>();
+            Mock<INotaFiscalRepository> notaFiscalRepository = new Mock<INotaFiscalRepository>();
+            Mock<IEmiteNotaFiscalContingenciaFacade> emiteNotaFiscalContingenciaService = new Mock<IEmiteNotaFiscalContingenciaFacade>();
+            Mock<IEmitenteRepository> emissorService = new Mock<IEmitenteRepository>();
+            Mock<IConsultarNotaFiscalService> nfeConsulta = new Mock<IConsultarNotaFiscalService>();
+            Mock<IServiceFactory> serviceFactory = new Mock<IServiceFactory>();
+            Mock<CertificadoService> certificadoService = new Mock<CertificadoService>();
+            Mock<SefazSettings> sefazSettings = new Mock<SefazSettings>();
+
+            configuracaoRepository.Setup(c => c.GetConfiguracao()).Returns(new ConfiguracaoEntity());
+            consultaStatusServicoService.Setup(c => c.ExecutarConsultaStatus(It.IsAny<ConfiguracaoEntity>(), It.IsAny<Modelo>())).Returns(true);
+
+            var contingenciaActor = CreateTestProbe();
+
+            var subject = Sys.ActorOf(Props.Create(() => new ModoOnlineActor(
+                configuracaoRepository.Object, consultaStatusServicoService.Object, notaFiscalRepository.Object,
+                emiteNotaFiscalContingenciaService.Object, emissorService.Object, nfeConsulta.Object,
+                serviceFactory.Object, certificadoService.Object, sefazSettings.Object, _ => contingenciaActor.Ref)));
+
+            // When
+            subject.Tell(new ModoOnlineActor.Start());
+
+            // Then
+            contingenciaActor.ExpectMsg<EmiteNFeContingenciaActor.TransmitirNFeEmContingencia>();
+            configuracaoRepository.Verify(c => c.Salvar(It.Is<ConfiguracaoEntity>(config => config.IsContingencia == false)), Times.Once);
+            configuracaoRepository.Verify(c => c.GetConfiguracao(), Times.Exactly(2));
         }
     }
 }
