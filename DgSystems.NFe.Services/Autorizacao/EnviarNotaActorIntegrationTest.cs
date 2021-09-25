c2fa8f632bb4615db104d67a0bd24f31f445e6d5
Domício Medeiros <domicioam@gmail.com>
2021-09-25 18:57:35 +0100

    ...

---
+++ DgSystems.NFe.Services/Autorizacao/EnviarNotaActorIntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using Akka.TestKit.Xunit2;
+using DgSystems.NFe.Core.UnitTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DgSystems.NFe.Services.UnitTests.Autorizacao
+{
+    public class EnviarNotaActorIntegrationTest : TestKit, IClassFixture<NotaFiscalFixture>
+    {
+        [Fact]
+        public void Deve_enviar_nota_em_contingencia_quando_offline()
+        {
+            // ativa modo offline
+            // envia nota para enviar nota actor
+            // mensagem deve ser encaminhada para nota fiscal contingencia actor
+        }
+    }
+}
